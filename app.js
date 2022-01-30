8c34181752c9b802003e947ebc4392c485749c48
Evan Hayes <hayesevan7@gmail.com>
2022-01-30 09:57:37 -0500

    added user action and change player functions

--- app.js
+++ app.js
@@ -1,31 +1,84 @@
-const tiles = Array.from(document.querySelectorAll(".tile"));
-const spanPlayerDisplay = document.querySelector(".display-player");
-const resetButton = document.getElementById("reset");
-const annoucer = document.querySelector(".display-annoucer")
+window.addEventListener("DOMContentLoaded", () => {
+    const tiles = Array.from(document.querySelectorAll(".tile"));
+    const spanPlayerDisplay = document.querySelector(".display-player");
+    const resetButton = document.getElementById("reset");
+    const annoucer = document.querySelector(".display-annoucer")
 
-let gameBoard = ['','','','','','','','',''];
-let currentPlayer = "X";
-let isGameActive = true;
+    let gameBoard = ['','','','','','','','',''];
+    let currentPlayer = "X";
+    let isGameActive = true;
 
-const playerXWins = "Player X Won!";
-const playerOWins = "PLayer O Won!";
-const tie = "Tie!"
+    const playerXWins = "Player X Won!";
+    const playerOWins = "PLayer O Won!";
+    const tie = "Tie!"
 
-/* 
-    Board Indexes
-    [0][1][2]
-    [3][4][5]
-    [6][7][8]
+    /* 
+        Board Indexes
+        [0][1][2]
+        [3][4][5]
+        [6][7][8]
+    */
 
-*/
+    const winningPositions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
 
-const winningPositions = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-];
+    const changePlayer = () => {
+        spanPlayerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === "X" ? "O":"X";
+        spanPlayerDisplay = currentPlayer;
+        spanPlayerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.Add(`player${currentPlayer}`);
+            updateboard(index);
+            handleValidation();
+            changePlayer();
+        }
+    }
+
+
+
+
+
+    tiles.forEach((tile, index) => {
+        tile.addEventListener("click", () => userAction(tile, index));
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
